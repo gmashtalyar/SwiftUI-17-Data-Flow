5382b714c574c8c785326b4f721d750be2d1b67a
gmashtalyar <gmashtalyar@yandex.ru>
2024-02-09 17:30:31 +0300

    Initial Commit

---
+++ Data Flow Tutorial/Data_Flow_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_Flow_TutorialApp.swift
+//  Data Flow Tutorial
+//
+//  Created by Геннадий Машталяр on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Data_Flow_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
